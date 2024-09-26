3e726bfa57ed402054ba9d780f6bfae86585f873
Luiggyvega <lsvegab@ufpso.edu.co>
2024-09-26 13:26:39 -0500

    promedio del grupo

--- Practica/untitled/src/Resultado/Resultado.java
+++ Practica/untitled/src/Resultado/Resultado.java
@@ -18,6 +18,7 @@ public class Resultado {
         do {
             System.out.println("Ingrese 1 para crear estudiante");
             System.out.println("ingrese 2 para mostar estudiantes y notas");
+            System.out.println("ingrese 3 para mostrar promedio de notas del grupo");
             System.out.println("Ingrese s para salir");
             System.out.println("Ingrese su opción");
             opcion = sc.next().charAt(0);
@@ -25,12 +26,27 @@ public class Resultado {
             switch (opcion) {
                 case '1' -> crearEstudiante();
                 case '2' -> mostrarEstudiante();
+                case '3' -> mostrarPromedio();
                 default -> {
                     opcion = 's';
                     System.out.println("Hasta mañana");
                 }
             }
         } while (opcion != 's');
+    }
+    public static void mostrarPromedio(){
+        double promedio = 0;
+        for (Estudiantes est : listaEstudiantes) {
+            double definitiva = est.getNotasNuevas();
+            for (int i = 0; i < listaEstudiantes.size(); i++) {
+                double cantidad = listaEstudiantes.size();
+                definitiva++;
+                promedio=definitiva/cantidad;
+                System.out.println("la cantidad de estudiantes es: " + cantidad);
+            }
+        }
+        System.out.println("el promedio es: " + promedio);
+
     }
     public static void mostrarEstudiante() {
 
